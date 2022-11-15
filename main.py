65f89219bcc1e23beb8a52cc93ce9875a8ecd2c0
Priyanka Dey <pdey3@illinois.edu>
2022-11-14 21:46:43 -0500

    update load data

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from svm import SVM
 import pandas as pd 
 
 #df = pd.read_csv("./diabetes.csv")
-df = pd.read_csv("./iris.csv") 
-dl = DataLoader(df.iloc[:, :-1].values, df.iloc[:,-1].values)
+#df = pd.read_csv("./iris.csv") 
+dl = DataLoader("./diabetes.csv") #df.iloc[:, :-1].values, df.iloc[:,-1].values)
 X_train, X_test, y_train, y_test = dl.load_data()
 logr = LogisticRegression()
 logr.fit(X_train, y_train) 
